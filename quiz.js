03f332b5d7b4f847c2fa5a3fa58565cc95487691
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-06-22 18:43:03 +0300

    add js logic to check answer

---
+++ quiz.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded",function(){
+        function checkAnswer(){
+            const correctAnswer = "4";
+
+            const selectedOption = document.querySelector('input[name="quiz"]:checked');
+            const userAnswer = selectedOption.value ;
+
+            if(userAnswer === correctAnswer){
+            const feedback = document.getElementById("feedback");
+            feedback.textContent = "Correct! Well done."
+           }else {
+              feedback.textContent = "That's incorrect. Try again!"
+           }
+        }
+        const submit = document.getElementById("submit-answer");
+        submit.addEventListener("click", checkAnswer);
+}); 
